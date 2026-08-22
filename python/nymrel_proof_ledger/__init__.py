fce361aafe6d858359516b0a158e97fa721a5eef
JalenBuildsHub <contact@jalenbuilds.com>
2026-08-21 20:10:17 -0700

    feat(signal): export the Python Signal profile API

--- python/nymrel_proof_ledger/__init__.py
+++ python/nymrel_proof_ledger/__init__.py
@@ -13,6 +13,19 @@ from .canonical import canonicalize, canonical_hash
 from .merkle import MerkleTree, hash_leaf, hash_nodes, MerkleProofStep
 from .signer import ProofSigner
 from .receipt import create_receipt, verify_receipt, hash_artifact, get_git_context
+from .signal import (
+    SIGNAL_PROOF_PROFILE,
+    SIGNAL_PROOF_PROFILE_VERSION,
+    SIGNAL_PROOF_BUNDLE_PROFILE,
+    SIGNAL_PROOF_BUNDLE_VERSION,
+    SIGNAL_PROOF_ENVELOPE_PATH,
+    SIGNAL_ATTESTED_SCOPES,
+    validate_signal_proof_envelope,
+    canonicalize_signal_proof_envelope,
+    digest_signal_proof_envelope,
+    create_signal_proof_bundle,
+    verify_signal_proof_bundle,
+)
 from .qr import generate_qr_matrix, generate_qr_svg_path
 from .badge import generate_svg_badge, generate_shield_svg, generate_html_certificate
 
@@ -40,4 +53,15 @@ __all__ = [
     "generate_qr_svg_path",
     "hash_artifact",
     "get_git_context",
+    "SIGNAL_PROOF_PROFILE",
+    "SIGNAL_PROOF_PROFILE_VERSION",
+    "SIGNAL_PROOF_BUNDLE_PROFILE",
+    "SIGNAL_PROOF_BUNDLE_VERSION",
+    "SIGNAL_PROOF_ENVELOPE_PATH",
+    "SIGNAL_ATTESTED_SCOPES",
+    "validate_signal_proof_envelope",
+    "canonicalize_signal_proof_envelope",
+    "digest_signal_proof_envelope",
+    "create_signal_proof_bundle",
+    "verify_signal_proof_bundle",
 ]
